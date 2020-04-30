3bfc02ce3e66d8c2de8d34b976828a0448970f75
MarkarianFR <silvere.noble@gmail.com>
2020-04-30 11:16:57 +0200

    conventions + config file

---
+++ config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Set status as "Ok" if you want to activate the launcher. If it's something else, the launcher will be disabled.
+//example: launcherStatus = "Sorry the launcher is under maintenance";
+
+module.exports = {
+    launcherStatus: "Ok"
+};
+
